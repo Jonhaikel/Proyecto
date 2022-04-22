e57bd571a415b01afa4db0013c9ff3713526be41
Jonhaikel <jonhaikel12@hotmail.com>
2022-04-22 16:23:19 -0600

    Finalizado

--- JS/Carrito.js
+++ JS/Carrito.js
@@ -1,5 +1,5 @@
-localStorage.setItem("Cliente",'Jonhaikel');
-localStorage.setItem("Correo",'Jonh@hotmail.com');
+localStorage.setItem("Cliente",'Dehjam');
+localStorage.setItem("Correo",'Dehjam@hotmail.com');
 window.onload = function(){
     document.getElementById('cliente').value =localStorage.getItem("Cliente");
     document.getElementById('correo').value =localStorage.getItem("Correo");
